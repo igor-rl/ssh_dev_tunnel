8f830807598520285636f7f13278f5b935825795
Igor Lage <igor-rl@hotmail.com>
2026-03-24 21:07:20 -0300

    v3.2: uninstall

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ssh-dev-tunnel",
-    version="3.1",
+    version="3.2",
     author="Igor Lage",
     packages=find_packages(),
     entry_points={
