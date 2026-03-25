6fc6f90263ef2932ce1dccf47651618d996bca4d
Igor Lage <igor-rl@hotmail.com>
2026-03-24 21:10:49 -0300

    v3.3: gitflow

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ssh-dev-tunnel",
-    version="3.2",
+    version="3.3",
     author="Igor Lage",
     packages=find_packages(),
     entry_points={
