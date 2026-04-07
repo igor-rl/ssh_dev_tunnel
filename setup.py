245cbd82600540c78e2d5a5257b54213500546d2
Igor Lage <igor-rl@hotmail.com>
2026-04-07 10:00:39 -0300

    v3.6.4: fix build

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ssh-dev-tunnel",
-    version="3.6.2",
+    version="3.6.4",
     author="Igor Lage",
     packages=find_packages(),
     install_requires=[
