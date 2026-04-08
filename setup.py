8332a48dcb10fa08775bc04f6492ee41521bcd8a
Igor Lage <igor-rl@hotmail.com>
2026-04-08 09:15:02 -0300

    v3.6.24: multiple tunnel

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ssh-dev-tunnel",
-    version="3.6.23",
+    version="3.6.24",
     author="Igor Lage",
     packages=find_packages(),
     python_requires=">=3.8",
